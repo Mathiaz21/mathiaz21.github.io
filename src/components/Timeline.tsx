12cc1861e2df021435cd58010adea5409c59c59c
Mathiaz21 <mathias.gilbert42@gmail.com>
2025-07-13 11:26:45 +0200

    Minor commit to keep the streak going and plannifying the next steps to take with TODOs

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -19,11 +19,13 @@ const Timeline: React.FC = () => {
   };
 
   return (
+    // TODO: make it possible to toggle between both columns, make it possible to add more different timelines
+    // For instance a Timeline for student clubs or past projects that weren't on GitHub, such as the Dragon's 
+    // game I made with Marc, or the TKinter processor simulator
     <section className="my-10 w-full max-w-5xl mx-auto px-4">
-      <h2 className="text-2xl font-bold text-center mb-10">My Timeline</h2>
+      <h2 className="text-2xl font-bold text-center mb-10">My Timelines</h2>
       
       <div className="flex justify-center gap-12">
-        {/* Professional Column */}
         <div className="flex gap-4 h-full">
           <div className="flex flex-col items-end">
             <h3 className="text-xl font-semibold mb-6 text-right">Professional</h3>
@@ -45,7 +47,6 @@ const Timeline: React.FC = () => {
           </div>
         </div>
         
-        {/* Academic Column */}
         <div className="flex gap-4 h-full">
           <div className="flex flex-col items-start">
             <h3 className="text-xl font-semibold mb-6">Academic</h3>
